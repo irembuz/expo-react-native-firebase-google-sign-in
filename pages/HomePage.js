0c7d7542597666d26be5487b5ccb87196481bfbb
irembuz <irembuz7@gmail.com>
2023-11-06 14:49:57 +0300

    onGoogleButtonPress handler

--- pages/HomePage.js
+++ pages/HomePage.js
@@ -1,13 +1,32 @@
 import { Button, StyleSheet, Text, View } from "react-native";
+import { GoogleAuthProvider, signInWithCredential } from "firebase/auth";
+import { auth } from "../firebaseConfig";
 
 const HomePage = ({ navigation }) => {
+  const onGoogleButtonPress = async () => {
+    try {
+      // TODO: Add Google Signin which fetches the idToken
+
+      if (idToken) {
+        // Create a Google credential with the token
+        const credential = GoogleAuthProvider.credential(idToken);
+
+        signInWithCredential(auth, credential).then((result) => {
+          if (result && result.user) {
+            navigation.navigate("Dashboard");
+          }
+        });
+      }
+    } catch (error) {
+      console.log("Error in GoogleSignin");
+      return;
+    }
+  };
+
   return (
     <View style={styles.homePage}>
       <Text>HomePage</Text>
-      <Button
-        title="Go to Dashboard"
-        onPress={() => navigation.navigate("Dashboard")}
-      />
+      <Button title="Continue with Google" onPress={onGoogleButtonPress} />
     </View>
   );
 };
