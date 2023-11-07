cda37bb32370deab3345efdc7852bc777348ad95
irembuz <irembuz7@gmail.com>
2023-11-07 09:37:37 +0300

    Google Sign in via @react-native-google-signin/google-signin

--- pages/HomePage.js
+++ pages/HomePage.js
@@ -1,11 +1,26 @@
 import { Button, StyleSheet, Text, View } from "react-native";
+
 import { GoogleAuthProvider, signInWithCredential } from "firebase/auth";
-import { auth } from "../firebaseConfig";
+import { GoogleSignin } from "@react-native-google-signin/google-signin";
+
+import { auth, REACT_APP_WEB_CLIENT_ID } from "../firebaseConfig";
+
+GoogleSignin.configure({
+  webClientId: REACT_APP_WEB_CLIENT_ID,
+});
 
 const HomePage = ({ navigation }) => {
   const onGoogleButtonPress = async () => {
     try {
       // TODO: Add Google Signin which fetches the idToken
+
+      // Check if your device supports Google Play
+      await GoogleSignin.hasPlayServices({
+        showPlayServicesUpdateDialog: true,
+      });
+
+      // Get the user's ID token
+      const { idToken } = await GoogleSignin.signIn();
 
       if (idToken) {
         // Create a Google credential with the token
@@ -15,10 +30,13 @@ const HomePage = ({ navigation }) => {
           if (result && result.user) {
             navigation.navigate("Dashboard");
           }
+        })
+        .catch((error) => {
+          console.log("Error in Firebase signInWithCredential", error.message);
         });
       }
     } catch (error) {
-      console.log("Error in GoogleSignin");
+      console.log("Error in GoogleSignin", error.message);
       return;
     }
   };
